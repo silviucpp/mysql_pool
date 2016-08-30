95e96c6323d82f6354f55361cd440d6a2c45e32d
silviu <silviu.caragea@woowteam.com>
2016-08-30 13:49:31 +0300

    Remove the pooler supervisor start.

--- src/mysql_pool_sup.erl
+++ src/mysql_pool_sup.erl
@@ -9,17 +9,13 @@ start_link() ->
 
 init([]) ->
     Childrens = [
-        worker(mysql_connection_manager, infinity),
-        supervisor(pooler_sup, infinity)
+        worker(mysql_connection_manager, infinity)
     ],
 
     {ok, {{one_for_one, 10, 10}, Childrens}}.
 
 worker(Name, WaitForClose) ->
     children(Name, WaitForClose, worker).
-
-supervisor(Name, WaitForClose) ->
-    children(Name, WaitForClose, supervisor).
 
 children(Name, WaitForClose, Type) ->
     {Name, {Name, start_link, []}, permanent, WaitForClose, Type, [Name]}.
