cd02ab5e888f2b7cac9d94ac1cac9181c78bdcc7
silviu <silviu.caragea@woowteam.com>
2016-08-31 13:57:36 +0300

    Add support for json utils

--- src/mysql_utils.erl
+++ src/mysql_utils.erl
@@ -1,7 +1,15 @@
 -module(mysql_utils).
 -author("silviu").
 
--export([env/1, lookup/2, lookup/3, replace/3, delete/2]).
+-export([
+    env/1,
+    lookup/2,
+    lookup/3,
+    replace/3,
+    delete/2,
+    as_json/1,
+    as_json/2
+]).
 
 env(Key) ->
     application:get_env(mysql_pool, Key).
@@ -27,3 +35,23 @@ replace(Key, NewValue, List) ->
 
 delete(Key, List) ->
     lists:keydelete(Key, 1, List).
+
+as_json({ok, Columns, Rows}) ->
+    as_json(Columns, Rows).
+
+as_json(Columns, Rows) ->
+    [begin [{K, json_val(V)} || {K, V} <- lists:zip(Columns, Row)] end || Row <- Rows].
+
+json_val(Value) when is_tuple(Value) ->
+    % convert datetime, date, time into string
+    % decimals seconds are not supported yet. so for the moment are truncated.
+    case Value of
+        {{Year, Month ,Day}, {Hour, Min, Sec}} ->
+            iolist_to_binary(io_lib:format("~4.4.0w-~2.2.0w-~2.2.0wT~2.2.0w:~2.2.0w:~2.2.0wZ", [Year, Month, Day, Hour, Min, trunc(Sec)]));
+        {Year, Month, Day} ->
+            iolist_to_binary( io_lib:format("~4.4.0w-~2.2.0w-~2.2.0w", [Year, Month, Day]));
+        {Days, {Hour, Min, Sec}} ->
+            iolist_to_binary( io_lib:format("~2.2.0w:~2.2.0w:~2.2.0w", [Days *24+Hour, Min, trunc(Sec)]))
+    end;
+json_val(Value) ->
+    Value.
