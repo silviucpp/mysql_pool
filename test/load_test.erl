88db5867e0122ec09d0fdb97ef70396fd373d5a2
Silviu Caragea <silviu.caragea@woowteam.com>
2018-07-01 01:03:18 +0300

    Update benchmark script

--- test/load_test.erl
+++ test/load_test.erl
@@ -5,10 +5,14 @@
 %%       `name` VARCHAR(100) NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB
 
 -export([
-    bench/2
+    bench/2,
+    bench/3
 ]).
 
 bench(Number, Concurrency) ->
+    bench(Number, Concurrency, false).
+
+bench(Number, Concurrency, Profile) ->
     case mysql_pool:start() of
         ok ->
             mysql_pool:prepare(mypool, get_accounts, <<"SELECT id, name FROM users WHERE id = ?">>);
@@ -22,10 +26,12 @@ bench(Number, Concurrency) ->
 
     Fun = fun() -> {ok, _, _} = mysql_pool:execute(mypool, get_accounts, [1]) end,
 
+    profilers_start(Profile),
     A = os:timestamp(),
     Pids = [spawn_link(fun() -> loop(LoopNumbers, Fun), Self ! {self(), done} end) || _ <- List],
     [receive {Pid, done} -> ok end || Pid <- Pids],
     B = os:timestamp(),
+    profiler_stop(Profile),
 
     print(Number, A, B).
 
@@ -46,3 +52,16 @@ loop(0, _Fun) ->
 loop(Nr, Fun) ->
     Fun(),
     loop(Nr-1, Fun).
+
+profilers_start(true)->
+    {ok, P} = eprof:start(),
+    eprof:start_profiling(processes() -- [P]);
+profilers_start(_)->
+    ok.
+
+profiler_stop(true)->
+    eprof:stop_profiling(),
+    eprof:analyze(total),
+    eprof:stop();
+profiler_stop(_)->
+    ok.
